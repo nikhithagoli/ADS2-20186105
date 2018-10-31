4e990b68c6c511c57305d10f1e0b43778ce12f6b
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-10-31 11:36:49 +0530

    "ads2_m2_a1_1: 0/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m2/2.1 CC_Percolation/Solution.java
+++ assignments/m2/2.1 CC_Percolation/Solution.java
@@ -1,176 +1,160 @@
 import java.util.Scanner;
+
 /**
  * Class for percolation.
  */
 class Percolation {
-  /**
-   * declaration of wqu.
-   */
-  private Graph g;
-  /**
-   * declaration of n.
-   */
-  private int n;
-  /**
-   * declaration of size.
-   */
-  private int size;
-  /**
-   * declaration of first.
-   */
-  private int first;
-  /**
-   * declaration of last.
-   */
-  private int last;
-  /**
-   * declaration of connected.
-   */
-  private boolean[] connected;
-  /**
-   * declaration of count.
-   */
-  private int count;
-  /**
-   * Constructs the object.
-   *
-   * @param      n1    The n 1
-   */
-   Percolation(final int n1) {
-    this.n = n1;
-    this.count = 0;
-    this.size = n1 * n1;
-    this.first = size;
-    this.last = size + 1;
-    connected = new boolean[size];
-    g = new Graph(size + 2);
-    for (int i = 0; i < n; i++) {
-      g.addEdge(first, i);
-      g.addEdge(last, size - i - 1);
+    /**
+     * grid.
+     */
+    private Graph g;
+    /**
+     * n.
+     */
+    private int n;
+    /**
+     * top.
+     */
+    private int top;
+    /**
+     * bottom.
+     */
+    private int bottom;
+    /**
+     * size.
+     */
+    private int size;
+    /**
+     * connected.
+     */
+    private boolean[][] connected;
+    /**
+     * open count.
+     */
+    private int opencount;
+    /**
+     * Constructs the object.
+     *
+     * @param      n     { parameter_description }
+     */
+    Percolation(final int n1) {
+        n = n1;
+        opencount = 0;
+        size = n1 * n1;
+        top = size;
+        bottom = size + 1;
+        connected = new boolean[n][n];
+        g = new Graph(size + 2);
+        
     }
-   }
-   /**
-    * Searches for the first match.
-    *
-    * @param      i     { parameter_description }
-    * @param      j     { parameter_description }
-    *
-    * @return     { description_of_the_return_value }
-    */
-   public int indexOf(final int i, final int j) {
-      return n * (i - 1) + j - 1;
-   }
-   /**
-    * Links open sites.
-    *
-    * @param      row   The row
-    * @param      col   The col
-    */
-   private void linkOpenSites(final int row, final int col) {
-    if (connected[col] && !g.hasEdge(row, col)) {
-      g.addEdge(row, col);
-    }
-   }
-   /**
-    * numberOfOpenSites.
-    *
-    * @return     { description_of_the_return_value }
-    */
-   public int numberOfOpenSites() {
-    return count;
-   }
-   /**
-    * open.
-    *
-    * @param      row   The row
-    * @param      col   The col
-    */
-   public void open(final int row, final int col) {
-    int index = indexOf(row, col);
-    connected[index] = true;
-    count++;
-    int bottom = index + n;
-    int top = index - n;
-    if (n == 1) {
-      g.addEdge(first, index);
-      g.addEdge(last, index);
-
-    }
-    if (bottom < size) {
-      linkOpenSites(index, bottom);
-    }
-    if (top >= 0) {
-      linkOpenSites(index, top);
-    }
-    if (col == 1) {
-      if (col != n) {
-        linkOpenSites(index, index + 1);
-      }
-      return;
-    }
-    if (col == n) {
-      linkOpenSites(index, index - 1);
-      return;
-    }
-    linkOpenSites(index, index + 1);
-    linkOpenSites(index, index - 1);
-
-
-   }
-   /**
-    * Determines if open.
-    *
-    * @param      row   The row
-    * @param      col   The col
-    *
-    * @return     True if open, False otherwise.
-    */
-   public boolean isOpen(final int row, final int col) {
-    return connected[indexOf(row, col)];
-   }
-   /**
-    * percolates.
-    *
-    * @return     { description_of_the_return_value }
-    */
-   public boolean percolates() {
-    CC cc = new CC(g);
-    return cc.connected(first, last);
-   }
-}
-
-/**
- * Class for solution.
- */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused constructor.
-	}
-	/**
-	 * main.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		// Graph grid = new Graph(n);
-		Percolation p = new Percolation(n);
-		/*while(sc.hasNextLine()) {
-			String[] input = sc.nextLine().split(" ");
-			grid.addEdge(Integer.parseInt(input[0]) - 1, Integer.parseInt(input[1]) - 1);
-		}*/
-		while (sc.hasNext()) {
-            int r = sc.nextInt();
-            int c = sc.nextInt();
-            p.open(r, c);
+    /**
+     * open.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     */
+    public void open(final int row, final int col) {
+        if (!isOpen(row, col)) {
+            connected[row - 1][col - 1] = true;
+            opencount++;
         }
-		// Connectedcomponents cc = new Connectedcomponents(grid);
-		// System.out.println(cc.ispercolated(grid));
-		System.out.println(p.percolates()
-      			&& p.numberOfOpenSites() != 0);
-	}
+        if (row == 1) {
+            g.addEdge(getIndex(row, col), top);
+        }
+        if (row == n) {
+            g.addEdge(getIndex(row, col), bottom);
+        }
+        if (col > 1 && isOpen(row, col - 1)) {
+            g.addEdge(getIndex(row, col), getIndex(row, col - 1));
+        }
+        if (col < n && isOpen(row, col + 1)) {
+            g.addEdge(getIndex(row, col), getIndex(row, col + 1));
+        }
+        if (row > 1 && isOpen(row - 1, col)) {
+            g.addEdge(getIndex(row, col), getIndex(row - 1, col));
+        }
+        if (row < n && isOpen(row + 1, col)) {
+            g.addEdge(getIndex(row, col), getIndex(row + 1, col));
+        }
+    }
+    /**
+     * Determines if open.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if open, False otherwise.
+     */
+    public boolean isOpen(final int row, final int col) {
+        return connected[row - 1][col - 1];
+    }
+    /**
+     * Determines if full.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if full, False otherwise.
+     */
+    public boolean isFull(final int row, final int col) {
+        if (0 < row && row <= n && 0 < col && col <= n) {
+            return g.hasEdge(top, getIndex(row, col));
+        }
+        return false;
+    }
+    /**
+     * opensites.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int numberOfOpenSites() {
+        return opencount;
+    }
+    /**
+     * percolates.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean percolates() {
+        CC cc = new CC(g);
+        return cc.connected(top, bottom);
+    }
+    /**
+     * getInex.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     The index.
+     */
+    private int getIndex(final int row, final int col) {
+        return n * (row - 1) + col;
+    }
+}
+/**
+ * soluton class.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //constructor.
+    }
+    /**
+     * main.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        Percolation p = new Percolation(n);
+        while (sc.hasNext()) {
+            String[] tokens = sc.nextLine().split(" ");
+            p.open(Integer.parseInt(tokens[0]),
+                    Integer.parseInt(tokens[1]));
+        }
+        System.out.println(p.percolates() && p.numberOfOpenSites() != 0);
+    }
 }

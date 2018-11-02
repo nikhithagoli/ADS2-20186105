5d14eeb06b19da0cf8789d63b5396b6c9ef8c668
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-11-02 17:11:47 +0530

    "ads2_m4_a1_1: 2/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String synsetsfile = sc.nextLine();
+		String hypernymsfile = sc.nextLine();
+		String mode =sc.nextLine();
+		if(mode.equals("Graph")) {
+			WordNet wordnet = new WordNet(synsetsfile, hypernymsfile);
+		}
+
+	}
+}

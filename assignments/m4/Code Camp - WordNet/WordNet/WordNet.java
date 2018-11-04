9795ef0e190c006a4af05de0519d94ab8f567f61
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-11-04 20:32:33 +0530

    "ads2_m4_a1_1: 14/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m4/Code Camp - WordNet/WordNet/WordNet.java
+++ assignments/m4/Code Camp - WordNet/WordNet/WordNet.java
@@ -1,72 +1,92 @@
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.ArrayList;
 public class WordNet {
-	private LinearProbingHashST<Integer, String> synsets;
-    private LinearProbingHashST<String, ArrayList<Integer>> ids;
-    private Digraph graph;
-    private SAP sap;
-    public WordNet(String s, String h) {
-        In syn = new In("Files//" + s);
-        synsets = new LinearProbingHashST<Integer, String>();
-        ids = new LinearProbingHashST<String, ArrayList<Integer>>(); 
-        syn = new In("Files\\" + s);
-        int id = 0;
-        while (syn.hasNextLine()) {
-            String[] tokens = syn.readLine().split(",");
-            id = Integer.parseInt(tokens[0]);
-            String[] words = tokens[1].split(" ");
-            for(String each: words) {
-                if(ids.contains(each)) {
-                    ids.get(each).add(id);
-                } else {
-                    ArrayList<Integer> a = new ArrayList<Integer>();
-                    a.add(id);
-                    ids.put(each, a);
+    private LinearProbingHashST <String, ArrayList<Integer>> nounLb;
+    private LinearProbingHashST <Integer, String> idLb;
+    private Digraph dg;
+    private SAP sp;
+
+    // constructor takes the name of the two input files
+    public WordNet(String synsets, String hypernyms) {
+        nounLb = new LinearProbingHashST<String, ArrayList<Integer>>();
+        idLb = new LinearProbingHashST<Integer, String>();
+        try {
+            In in = new In("./Files/" + synsets);
+            int id = 0;
+            while (!in.isEmpty()) {
+                String[] tokens = in.readLine().split(",");
+                id = Integer.parseInt(tokens[0]);
+                String[] words = tokens[1].split(" ");
+                ArrayList<String> wordList = new ArrayList<String>();
+                for (String noun : words) {
+                    wordList.add(noun);
+                }
+                idLb.put(id, tokens[1]);
+                for (String noun : words) {
+                    if (nounLb.contains(noun)) {
+                        nounLb.get(noun).add(id);
+                    } else {
+                        ArrayList<Integer> list = new ArrayList<Integer>();
+                        list.add(id);
+                        nounLb.put(noun, list);
+                    }
                 }
             }
-            synsets.put(Integer.parseInt(tokens[0]), tokens[1]);
-        }
-        In hype = new In("Files\\" + h);
-        graph = new Digraph(id + 1);
-        while (hype.hasNextLine()) {
-            String[] tokens = hype.readLine().split(",");
-            for (int i = 1; i < tokens.length; i++) {
-                graph.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[i]));
+            this.dg = new Digraph(id + 1);
+            in = new In("./Files/" + hypernyms);
+            while (!in.isEmpty()) {
+                String[] tokens = in.readLine().split(",");
+                int synId = Integer.parseInt(tokens[0]);
+                for (int i = 1; i < tokens.length; i++) {
+                    dg.addEdge(synId, Integer.parseInt(tokens[i]));
+                }
             }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        sap = new SAP(graph);
+        sp = new SAP(dg);
     }
     public void print() {
-    	DirectedCycle dc = new DirectedCycle(graph);
+        DirectedCycle dc = new DirectedCycle(dg);
         if (dc.hasCycle()) {
             throw new IllegalArgumentException("Cycle detected");
-        } else if (graph.noOfOutdegree() > 1) {
+        } else if (dg.noOfOutdegree() > 1) {
             throw new IllegalArgumentException("Multiple roots");
         } else {
-            System.out.println(graph.toString());
+            System.out.println(dg.toString());
         }
     }
-    public Iterable<String> nouns() {
-        return ids.keys();
+public Iterable<String> nouns() {
+    return nounLb.keys();
+
+}
+
+//     // is the word a WordNet noun?
+public boolean isNoun(String word) {
+    return nounLb.contains(word);
+
+}
+
+//     // distance between nounA and nounB (defined below)
+public int distance(String nounA, String nounB) {
+    if (!isNoun(nounA) || !isNoun(nounB)) {
+        throw new IllegalArgumentException();
     }
-    public boolean isNoun(String word) {
-        return ids.contains(word);
-    }
-    public int distance(String nounA, String nounB) {
-        if (!isNoun(nounA) || !isNoun(nounB)) {
+    ArrayList<Integer> idA = nounLb.get(nounA);
+    ArrayList<Integer> idB = nounLb.get(nounB);
+    return sp.length(idA, idB);
+}
+
+//     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+//     // in a shortest ancestral path (defined below)
+public String sap(String nounA, String nounB) {
+    if (!isNoun(nounA) || !isNoun(nounB)) {
             throw new IllegalArgumentException();
         }
-        ArrayList<Integer> idA = ids.get(nounA);
-        ArrayList<Integer> idB = ids.get(nounB);
-        return sap.length(idA, idB);
-    }
-    public String sap(String nounA, String nounB) {
-        if (!isNoun(nounA) || !isNoun(nounB)) {
-            throw new IllegalArgumentException();
-        }
-        ArrayList<Integer> idA = ids.get(nounA);
-        ArrayList<Integer> idB = ids.get(nounB);
-        int ancestor = sap.ancestor(idA, idB);
-        return synsets.get(ancestor);
+        ArrayList<Integer> idA = nounLb.get(nounA);
+        ArrayList<Integer> idB = nounLb.get(nounB);
+
+        int anc = sp.ancestor(idA, idB);
+        return idLb.get(anc);
     }
 }

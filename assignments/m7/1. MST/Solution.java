7cf319ffc8ed102331ff2f2c1b2ba191c87d5181
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-11-05 18:08:12 +0530

    "ads2_m7_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignments/m7/1. MST/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int v = Integer.parseInt(sc.nextLine());
+		int e = Integer.parseInt(sc.nextLine());
+		EdgeWeightedGraph graph = new EdgeWeightedGraph(v, e);
+		/*for(int i = 0; i < edges; i++) {
+			String[] tokens = sc.nextLine().split(" ");
+
+		}*/
+		KruskalMST mst = new KruskalMST(graph);
+		System.out.println(mst.weight());
+	}
+}

ba96387cfb1ace49f00e51ed747e6365df67be84
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-11-06 18:25:06 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignments/m8/1. Shortest Paths/Solution.java
+++ assignments/m8/1. Shortest Paths/Solution.java
@@ -25,9 +25,18 @@ final class Solution {
         for (int i = 0; i < n; i++) {
             places.put(input[i], i);
         }
+        /* it creates an empty undirected graph in O(v) time
+         * where v is no of vertices.
+         * 
+         * and it takes O(E) time to add all the edges in the graph
+         * where E is no of edges.
+         * 
+         * so in total O(V + E)
+         */
         EdgeWeightedGraph graph = new EdgeWeightedGraph(n);
         for (int i = 0; i < m; i++) {
             String[] tokens = sc.nextLine().split(" ");
+            //it adds a new edge in O(1) time.
             graph.addEdge(new Edge(places.get(tokens[0]),
                                    places.get(tokens[1]),
                                    Double.parseDouble(tokens[2])));
@@ -35,8 +44,10 @@ final class Solution {
         int q = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < q; i++) {
             String[] tokens = sc.nextLine().split(" ");
+            /* it takes O(Elog(V)) time to form an dijkstra sp */
             DijkstraUndirectedSP sp = new DijkstraUndirectedSP(graph,
                     places.get(tokens[0]));
+            //its returns a distto value in O(1) time.
             System.out.println(Math.round(sp.distTo(places.get(tokens[1]))));
         }
     }

c47de575238d2bcd40cdc3c0d7423ad17a53acd6
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-11-06 18:10:06 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignments/m8/1. Shortest Paths/Solution.java
+++ assignments/m8/1. Shortest Paths/Solution.java
@@ -1,26 +1,42 @@
 import java.util.Scanner;
 import java.util.HashMap;
-class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		sc.nextLine();
-		HashMap<String, Integer> places = new HashMap<>();
-		String[] input = sc.nextLine().split(" ");
-		for(int i = 0; i < n; i++) {
-			places.put(input[i], i);
-		}
-		EdgeWeightedGraph graph = new EdgeWeightedGraph(n);
-		for(int i = 0; i < m; i++) {
-			String[] tokens = sc.nextLine().split(" ");
-			graph.addEdge(new Edge(places.get(tokens[0]), places.get(tokens[1]), Double.parseDouble(tokens[2])));
-		}
-		int q = Integer.parseInt(sc.nextLine());
-		for(int i = 0; i < q; i++) {
-			String[] tokens = sc.nextLine().split(" ");
-			DijkstraUndirectedSP sp = new DijkstraUndirectedSP(graph, places.get(tokens[0]));
-			System.out.println(Math.round(sp.distTo(places.get(tokens[1]))));
-		}
-	}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * main.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.nextLine();
+        HashMap<String, Integer> places = new HashMap<>();
+        String[] input = sc.nextLine().split(" ");
+        for(int i = 0; i < n; i++) {
+            places.put(input[i], i);
+        }
+        EdgeWeightedGraph graph = new EdgeWeightedGraph(n);
+        for(int i = 0; i < m; i++) {
+            String[] tokens = sc.nextLine().split(" ");
+            graph.addEdge(new Edge(places.get(tokens[0]),
+                 places.get(tokens[1]),
+                     Double.parseDouble(tokens[2])));
+        }
+        int q = Integer.parseInt(sc.nextLine());
+        for(int i = 0; i < q; i++) {
+            String[] tokens = sc.nextLine().split(" ");
+            DijkstraUndirectedSP sp = new DijkstraUndirectedSP(graph, places.get(tokens[0]));
+            System.out.println(Math.round(sp.distTo(places.get(tokens[1]))));
+        }
+    }
 }

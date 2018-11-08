16c24a44ed5424fb2ee04934cb8960ce2b0ade40
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-11-08 12:16:37 +0530

    "ads2_m10_a1_1: 5/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m10/Seam Carving/SeamCarver.java
+++ assignments/m10/Seam Carving/SeamCarver.java
@@ -4,7 +4,7 @@ public class SeamCarver {
 	private Picture picture;
 	private int width;
 	private int height;
-	public SeamCarver(Picture pic) {
+	public SeamCarver(Picture pic) throws IllegalArgumentException {
 		if (pic == null) {
 			throw new IllegalArgumentException("picture is null");
 		}

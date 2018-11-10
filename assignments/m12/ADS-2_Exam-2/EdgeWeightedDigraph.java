918d5a6653fe8baa72576143213caf4994a2eb86
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-11-10 12:10:13 +0530

    "ads2_m12_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m12/ADS-2_Exam-2/EdgeWeightedDigraph.java
+++ assignments/m12/ADS-2_Exam-2/EdgeWeightedDigraph.java
@@ -171,7 +171,7 @@ public class EdgeWeightedDigraph {
      */
     public String toString() {
         StringBuilder s = new StringBuilder();
-        s.append(V + " vertices " + E + " edges" + NEWLINE);
+        s.append(V + " vertices " + E/2 + " edges" + NEWLINE);
         for (int v = 0; v < V; v++) {
             s.append(v + ": ");
             for (DirectedEdge e : adj[v]) {

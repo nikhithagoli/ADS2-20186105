315c66aceeff8908f46a99083e21d1dc8dc8b3b9
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-11-14 16:39:09 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ assignments/m13/Radix Sorts/LSD.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+/**
+ * Class for lsd.
+ */
+public class LSD {
+    private static final int BITS_PER_BYTE = 8;
+
+    /**
+     * Constructs the object.
+     */
+    public LSD() { }
+
+   /**  
+     * Rearranges the array of W-character strings in ascending order.
+     *
+     * @param a the array to be sorted
+     * @param w the number of characters per string
+     */
+    public static void sort(final String[] a, final int w) {
+        int n = a.length;
+        int R = 256;   // extend ASCII alphabet size
+        String[] aux = new String[n];
+
+        for (int d = w-1; d >= 0; d--) {
+            // sort by key-indexed counting on dth character
+
+            // compute frequency counts
+            int[] count = new int[R+1];
+            for (int i = 0; i < n; i++)
+                count[a[i].charAt(d) + 1]++;
+
+            // compute cumulates
+            for (int r = 0; r < R; r++)
+                count[r+1] += count[r];
+
+            // move data
+            for (int i = 0; i < n; i++)
+                aux[count[a[i].charAt(d)]++] = a[i];
+
+            // copy back
+            for (int i = 0; i < n; i++)
+                a[i] = aux[i];
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}

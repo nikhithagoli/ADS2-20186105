d67e655f101f842e04d7a52680d1ebd9807bc673
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-11-14 16:44:16 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignments/m13/Radix Sorts/Solution.java
+++ assignments/m13/Radix Sorts/Solution.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
  * class for sloution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unsed.
-	}
-	/**
-	 * main.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		String[] strings = new String[n];
-		for(int i = 0; i < n; i++) {
-			strings[i] = sc.nextLine();
-		}
-		LSD lsd = new LSD();
-		lsd.sort(strings, strings[0].length());
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unsed.
+    }
+    /**
+     * main.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String[] strings = new String[n];
+        for (int i = 0; i < n; i++) {
+            strings[i] = sc.nextLine();
+        }
+        LSD lsd = new LSD();
+        lsd.sort(strings, strings[0].length());
+    }
 }

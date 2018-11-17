6c8122b1876ccc30905e2cbf37eecb3b7e7879c5
nikhitha goli 20186105 <nikhithagolinikki86@msitprogram.net>
2018-11-17 13:02:07 +0530

    "ads2_m18_a1_1: 3/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m18/ADS-2Exam-3/Solution.java
+++ assignments/m18/ADS-2Exam-3/Solution.java
@@ -235,7 +235,7 @@ class T9 {
             sortedfrequencies.insert((Integer) tst.get(each));
         }
         TreeSet<String> suggestions = new TreeSet<String>();
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < k && sortedfrequencies.size() > i; i++) {
             int value = sortedfrequencies.delMax();
             for (String word : words) {
                 if (value == (Integer) tst.get(word)) {
